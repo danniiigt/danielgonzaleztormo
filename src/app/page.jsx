9d521fde0aa1e739d1fca17383b950d473fc655d
DanielGT <danielgonzalezt13@gmail.com>
2023-05-21 17:39:36 +0200

    Primer commit

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components/Navbar";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
